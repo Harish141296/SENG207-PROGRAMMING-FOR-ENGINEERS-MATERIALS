b5b3a8e59e1fdcc113d8d3f7134190b06d6ec712
Harish Paramathmananda <156591946+Harish141296@users.noreply.github.com>
2024-09-18 10:31:27 +0530

    Update 04input_function.py
    Updated the input() for the explanation.

--- 04input_function.py
+++ 04input_function.py
@@ -1,1 +1,5 @@
 # Python input function
+name:str = input("Enter your name: ")
+print(f"Your name is : ", name)
+number:int = int(input("Enter the number: ")) # str to int conversion
+print(f" Your picked number is : ", number)
